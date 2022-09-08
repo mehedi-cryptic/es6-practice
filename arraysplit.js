dab7dbcf26f410f5a664323ccbd56d1604ca0cd1
mehedi-cryptic <dexterousmehedi977@gmail.com>
2022-09-08 14:43:49 +0600

    array split join splice

---
+++ arraysplit.js
@@ -0,0 +1,6 @@
+const numbers=[11,23,21,12,1,2,1,2];
+// const part =numbers.slice(2,5);
+// const removed=numbers.splice(2,3);
+// console.log(removed);
+const tog=numbers.join(" ");
+console.log(tog);
