c77c4d0d27b0ba80a553052d4373733945045d17
mehedi-cryptic <dexterousmehedi977@gmail.com>
2022-09-08 11:15:12 +0600

    double and tripple equal

---
+++ doubletripleequal.js
@@ -0,0 +1,7 @@
+const first=2;
+const second="2";
+if (first===second){
+    console.log("conditon is true");
+}
+else{
+    console.log("condition is false");}
--- undefined.js
+++ undefined.js
@@ -1,2 +1,10 @@
 const premik={name:"kuddus", age:22, guy: "smart"};
 console.log(premik.gf);
+let word=undefined;
+console.log(word);
+
+let ages=[11,22,1];
+console.log(ages[11]);
+
+let pakhi;
+console.log(pakhi);
