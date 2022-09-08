055551a54f6ba4f9c01cac6ac71c6666823630c8
mehedi-cryptic <dexterousmehedi977@gmail.com>
2022-09-08 13:03:25 +0600

    object property

---
+++ objectproperty.js
@@ -0,0 +1,25 @@
+const students=[
+    {
+        id:21,
+        name:"sunny"
+    },
+    {
+        id:31,
+        name:"kanna"
+    },
+    {
+        id:41,
+        name:"jaya"
+    },
+    {
+        id:82,
+        name:"jawdjk"
+    }
+
+];
+
+const name=students.map(s=>s.name);
+console.log(name);
+// for (let i=o; i<students.length; i++){
+
+// }
