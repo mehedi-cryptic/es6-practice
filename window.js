e15318718c3f882a91ba8167d571a0b893e46e7b
mehedi-cryptic <dexterousmehedi977@gmail.com>
2022-09-17 13:58:23 +0600

    async wait

---
+++ window.js
@@ -0,0 +1,10 @@
+var name="mm";
+function add(num1, num2){
+    var res=num1+num2;
+    console.log('name',name )
+    return res;
+}
+console.log(name);
+const sum=add(1,2);
+console.log(sum);
+///global variable will be access from anywhere
